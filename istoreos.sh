90e1baba385b9f1f2dfeb6f7c99c77fdd26c5b45
sinoserendipity <sinoserendipity@gmail.com>
2026-04-27 03:06:26 -0400

    Update FILE_NAME to new version of istoreos image

--- istoreos.sh
+++ istoreos.sh
@@ -3,7 +3,7 @@ mkdir -p openwrt
 
 REPO="wukongdaily/img-installer"
 TAG="2025-03-12"
-FILE_NAME="istoreos-24.10.1-2025060614-x86-64-squashfs-combined-efi.img.gz"
+FILE_NAME="istoreos-24.10.6-2026041710-x86-64-squashfs-combined-efi.img.gz"
 OUTPUT_PATH="openwrt/istoreos.img.gz"
 DOWNLOAD_URL=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG | jq -r '.assets[] | select(.name == "'"$FILE_NAME"'") | .browser_download_url')
 
